44edce99f5b7e73a9968792a9e8884e16c7b9f24
Alexandr <alex____98@hotmail.com>
2017-03-06 13:13:58 +0200

    Merge branch 'master' of D:\Apps\Joke with conflicts.

---
+++ app/src/main/java/ua/matvienko_apps/joke/Activity/FavoritesActivity.java
@@ -0,0 +1,15 @@
+package ua.matvienko_apps.joke.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ua.matvienko_apps.joke.R;
+
+public class FavoritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
